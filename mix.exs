a57080673bc236f6a5001337133db3d1f68c9625
Kevin Greene <kevin@spantree.net>
2015-10-21 21:42:21 -0400

    Added description

--- mix.exs
+++ mix.exs
@@ -3,7 +3,8 @@ defmodule DiceRoller.Mixfile do
 
   def project do
     [app: :dice_roller,
-     version: "1.0.0",
+     description: "An Elixir library for simulating dice rolls",
+     version: "1.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
